7ac9fbb0b5549295c8906c00f8b3d09b6b43cb5d
Shandler Rechenberger <83612169+ShandlerR@users.noreply.github.com>
2024-06-04 08:52:10 -0600

    Monster Code Uploaded

---
+++ src/sillyGame.erl
@@ -0,0 +1,23 @@
+-module(sillyGame).
+
+-export([start/0, playerAttack/1, monsterDatabase/1]). % Makes the function public, with 0 parameters
+
+start() ->
+    Pid = spawn(sillyGame, monsterDatabase, [#{health => 5}]).
+    
+monsterDatabase(MonsterStats) ->
+    receive
+        {Sender, Damage} ->
+        NewHealth = maps:get(health, MonsterStats) - Damage,
+        monsterdatabase(#{"health" => NewHealth}),
+        Sender ! {maps:get(health, MonsterStats), Damage}
+    end.
+
+playerAttack(Pid) ->
+    Pid ! {self(), 2},
+    receive 
+        {MonsterStats, Damage} ->
+            io:format("hurting the monster with ~p health by doing ~p damage!~n", [MonsterStats, Damage]);
+        _ -> 
+            io:format("Unknown Response")
+    end.
